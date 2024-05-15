4d004bfc5a391367794e1d77e6e97824bc85a32d
Tarek Ziade <tarek@ziade.org>
2024-05-15 13:42:21 +0200

    use multiproc

--- distilvit/curate.py
+++ distilvit/curate.py
@@ -4,15 +4,19 @@ Using Phi-3-mini-4k-instruct to transform captions from the flickr30k dataset.
 import re
 import platform
 import csv
-from pathlib import Path
 
-from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
-from datasets import load_dataset, DatasetDict
 from transformers.utils import logging
 
 logging.set_verbosity_error()
+#torch.set_num_threads(1)
 
+
+dataset_name = "nlphuji/flickr30k"
+# model_name = "mistralai/Mistral-7B-Instruct-v0.2"
+model_name = "meta-llama/Meta-Llama-3-8B-Instruct"
+# model_name = "microsoft/Phi-3-mini-4k-instruct"
+batch_size = 10
 
 def extract_text_with_backticks(input_string):
     pattern = r"```(.*?)```"
@@ -20,25 +24,6 @@ def extract_text_with_backticks(input_string):
     if match is None:
         return input_string
     return match.group(1).strip()
-
-
-class CSVDataSaver:
-    def __init__(self, filename="curation.csv"):
-        self.filename = filename
-        # Ensure the file exists and has the header if it's a new file
-        if not Path(self.filename).exists():
-            with open(self.filename, mode="w", newline="") as file:
-                writer = csv.writer(file, delimiter="|")
-                writer.writerow(["dataset", "original caption", "new caption"])
-
-    def add(self, model_id, original_caption, new_caption):
-        """Add a new row to the CSV file."""
-        with open(self.filename, mode="a", newline="") as file:
-            writer = csv.writer(file, delimiter="|")
-            writer.writerow([model_id, original_caption, new_caption])
-
-
-csv_saver = CSVDataSaver()
 
 
 def load_model_and_tokenizer(model_name, device):
@@ -50,11 +35,21 @@ def load_model_and_tokenizer(model_name, device):
             "trust_remote_code": True,
         }
     else:
-        kw = {"trust_remote_code": True}
+        kw = {"trust_remote_code": True,
 
-    model = AutoModelForCausalLM.from_pretrained(model_name, **kw)
+              }
+    
+    from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+
+    bnb_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_use_double_quant=True,
+            bnb_4bit_quant_type="nf4",
+            bnb_4bit_compute_dtype=torch.float16,
+    )
+    model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", quantization_config=bnb_config, **kw)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model.to(device)
+    #model.to(device)
     return model, tokenizer
 
 
@@ -76,73 +71,79 @@ Wrap the result between triple backticks.
 """
 
 
-def transform(captions, model, tokenizer, device):
-    transformed_captions = []
 
-    for caption in captions:
-        messages = [
-            {"role": "user", "content": PROMPT3 + caption},
+class Batcher:
+    def __init__(self):
+        if torch.cuda.is_available():
+            device = torch.device("cuda")
+            print("Using CUDA (Nvidia GPU).")
+        elif torch.backends.mps.is_available():
+            device = torch.device("mps")
+            print("Using MPS (Apple Silicon GPU).")
+        else:
+            device = torch.device("cpu")
+            print("Using CPU.")
+
+        self.device = device
+        self.model = None
+
+    def process_batch(self, batch):
+        if self.model is None:
+            self.model, self.tokenizer = load_model_and_tokenizer(model_name, self.device)
+
+        batch["original_caption"] = list(batch["caption"])
+        batch["caption"] = [
+            self.transform(captions) for captions in batch["caption"]
         ]
+        return batch
 
-        inputs = tokenizer.apply_chat_template(
-            messages, add_generation_prompt=True, return_tensors="pt"
-        ).to(device)
+    def transform(self, captions):
+        transformed_captions = []
 
-        with torch.no_grad():
-            outputs = model.generate(
-                inputs,
-                max_new_tokens=40,  # 32
-                # do_sample=True,
-                # num_beams=2,
+        for caption in captions:
+            messages = [
+                {"role": "user", "content": PROMPT3 + caption},
+            ]
+
+            inputs = self.tokenizer.apply_chat_template(
+                messages, add_generation_prompt=True, return_tensors="pt"
+            ).to(self.device)
+
+            with torch.no_grad():
+                outputs = self.model.generate(
+                    inputs,
+                    max_new_tokens=40,  # 32
+                    # do_sample=True,
+                    # num_beams=2,
+                )
+
+            result = self.tokenizer.decode(
+                outputs[0][inputs[0].size().numel() :], skip_special_tokens=True
             )
+            result = extract_text_with_backticks(result)
+            result = result.split("\n")[0].strip()
+            #print(f"{caption} -> {result}")
+            #csv_saver.add("flickr30k", caption, result)
 
-        result = tokenizer.decode(
-            outputs[0][inputs[0].size().numel() :], skip_special_tokens=True
-        )
-        result = extract_text_with_backticks(result)
-        result = result.split("\n")[0].strip()
-        print(f"{caption} -> {result}")
-        csv_saver.add("flickr30k", caption, result)
+            transformed_captions.append(result)
 
-        transformed_captions.append(result)
+        return transformed_captions
 
-    return transformed_captions
-
-
-def process_batch(batch, model, tokenizer, device):
-    batch["original_caption"] = list(batch["caption"])
-    batch["caption"] = [
-        transform(captions, model, tokenizer, device) for captions in batch["caption"]
-    ]
-    return batch
 
 
 def main(test_sample=False):
-    if torch.cuda.is_available():
-        device = torch.device("cuda")
-        print("Using CUDA (Nvidia GPU).")
-    elif torch.backends.mps.is_available():
-        device = torch.device("mps")
-        print("Using MPS (Apple Silicon GPU).")
-    else:
-        device = torch.device("cpu")
-        print("Using CPU.")
+    from datasets import load_dataset, DatasetDict
 
-    dataset_name = "nlphuji/flickr30k"
-    # model_name = "mistralai/Mistral-7B-Instruct-v0.2"
-    model_name = "meta-llama/Meta-Llama-3-8B-Instruct"
-
-    # model_name = "microsoft/Phi-3-mini-4k-instruct"
-    batch_size = 10
-
-    model, tokenizer = load_model_and_tokenizer(model_name, device)
     split = "test[:100]" if test_sample else "test"
     dataset = load_dataset(dataset_name, split=split)
 
+    batcher = Batcher()
+
     dataset = dataset.map(
-        lambda batch: process_batch(batch, model, tokenizer, device),
+        batcher.process_batch, 
         batched=True,
         batch_size=batch_size,
+        num_proc=4
     )
 
     dataset_dict = DatasetDict({"test": dataset})
@@ -152,4 +153,4 @@ def main(test_sample=False):
 
 
 if __name__ == "__main__":
-    main(test_sample=True)
+    main(test_sample=False)
