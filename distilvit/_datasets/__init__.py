bfb1fb10500056d6c1cc0849878348665f42c356
Tarek Ziadé <tarek@ziade.org>
2024-05-27 11:08:57 +0200

    enable the validation ds

--- distilvit/_datasets/__init__.py
+++ distilvit/_datasets/__init__.py
@@ -1,6 +1,7 @@
 from .coco import get_dataset as get_coco_dataset
 from .flickr import get_dataset as get_flickr_dataset
 from .docornot import get_dataset as get_docornot_dataset
+from .validation import get_dataset as get_validation_dataset
 
 # from .textcaps import get_dataset as get_textcaps_dataset
 
@@ -9,4 +10,5 @@ DATASETS = {
     # "coco": get_coco_dataset,
     "docornot": get_docornot_dataset,
     # "textcaps": get_textcaps_dataset,
+    "validation": get_validation_dataset,
 }
