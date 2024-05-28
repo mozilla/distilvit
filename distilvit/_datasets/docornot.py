b9ef6f4d2f37aa5ec1165d2c0a1024fc064cb313
Tarek Ziadé <tarek@ziade.org>
2024-05-28 08:46:11 +0200

    fix the doc dataset

--- distilvit/_datasets/docornot.py
+++ distilvit/_datasets/docornot.py
@@ -9,11 +9,11 @@ from distilvit.utils import DatasetTokenizer, cached_ds
 def get_dataset(feature_extractor_model, text_decoder_model):
     from datasets import load_dataset
 
-    ds = load_dataset("mozilla/docornot")
+    ds = load_dataset("mozilla/docornot", split="train[:2000]")
 
     # we're only interested in documents and provide a fixed caption.
     ds = ds.filter(lambda example: example["is_document"] == 1)
-    ds = ds.map(lambda _: {"caption": ["The image seems to be a textual document."]})
+    ds = ds.map(lambda _: {"caption": ["Text document."]})
     ds = ds.remove_columns("is_document")
 
     ds_tokenizer = DatasetTokenizer(
@@ -23,5 +23,11 @@ def get_dataset(feature_extractor_model, text_decoder_model):
     )
 
     ds = ds_tokenizer("docornot", ds)
+
+    # splitting in train (80%), test (10%), validation (10%)
+    ds = ds.train_test_split(test_size=0.2)
+    test_and_eval = ds["test"].train_test_split(test_size=0.5)
+    ds["test"] = test_and_eval["test"]
+    ds["validation"] = test_and_eval["train"]
 
     return ds
